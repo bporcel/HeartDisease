e16e70ed142ea70cbefadf95bf25f9409eb3ba64
bporcel <codamming@gmail.com>
2021-01-19 08:38:44 +0100

    Pushing to repositoyry

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/hacko/dev/repositories/HeartDiseasePrediction/env/lib"
+XML2_LIBS="-lxml2 -L/home/hacko/dev/repositories/HeartDiseasePrediction/env/lib -lz -L/home/hacko/dev/repositories/HeartDiseasePrediction/env/lib -llzma -lpthread -L/home/hacko/dev/repositories/HeartDiseasePrediction/env/lib -L/home/hacko/dev/repositories/HeartDiseasePrediction/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/hacko/dev/repositories/HeartDiseasePrediction/env/include/libxml2 -I/home/hacko/dev/repositories/HeartDiseasePrediction/env/include"
+MODULE_VERSION="xml2-2.9.10"
+
